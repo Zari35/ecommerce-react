1a01d46023029ab0067379fc194ebc5fa41673b6
Sonya <sonya.nina.helal@gmail.com>
2023-01-31 10:35:15 +0300

    add sort filter for Books by Price and Rating

--- src/pages/Books.jsx
+++ src/pages/Books.jsx
@@ -1,6 +1,38 @@
-import React from 'react'
+import React, {useState} from 'react'
+import Book from '../components/ui/Book';
 
-export default function Books() {
+export default function Books( {books: initalBooks}) {
+    const [books, setBooks] = useState(initalBooks);
+
+    function filterBooks(filter) {
+        switch (filter) {
+          case "LOW_TO_HIGH":
+            return setBooks(
+              books
+                .slice()
+                .sort(
+                  (a, b) =>
+                    (a.salePrice || a.originalPrice) -
+                    (b.salePrice || b.originalPrice)
+                )
+            );
+          case "HIGH_TO_LOW":
+            return setBooks(
+              books
+                .slice()
+                .sort(
+                  (a, b) =>
+                    (b.salePrice || b.originalPrice) -
+                    (a.salePrice || a.originalPrice)
+                )
+            );
+          case "RATING":
+            return setBooks(books.slice().sort((a, b) => b.rating - a.rating));
+          default:
+            break;
+        }
+      }
+
   return (
     <div id="books__body">
       <main id="books__main">
@@ -11,7 +43,8 @@ export default function Books() {
                 <h2 className="section__title books__header--title">
                   All Books
                 </h2>
-                <select id="filter" defaultValue="DEFAULT">
+                <select id="filter" defaultValue="DEFAULT"
+                onChange={(event) => filterBooks(event.target.value)}>
                   <option value="DEFAULT" disabled>
                     Sort
                   </option>
@@ -19,6 +52,12 @@ export default function Books() {
                   <option value="HIGH_TO_LOW">Price, High to Low</option>
                   <option value="RATING">Rating</option>
                 </select>
+              </div>
+              <div className="books">
+                {
+                books.map(book => <Book book={book} key={book.id} />
+                )
+                }
               </div>
             </div>
           </div>
