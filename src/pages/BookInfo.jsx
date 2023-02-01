9f2013bce5252294ca7eed8200dfd172563ee2b3
Sonya <sonya.nina.helal@gmail.com>
2023-02-01 20:27:53 +0300

    adding Links with react-router-dom and adding BookInfo page !!fixing error of the page

---
+++ src/pages/BookInfo.jsx
@@ -0,0 +1,67 @@
+import React from "react";
+import { useParams } from "react-router";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Link } from "react-router-dom";
+
+const BookInfo = ({ books, addItemToCart }) => {
+  const { id } = useParams();
+  const book = books.find((book) => +book.id === +id);
+
+  return (
+    <div id="books__body">
+      <main id="books__main">
+        <div className="books__container">
+          <div className="row">
+            <div className="book__selected--top">
+              <Link to="/books" className="book__link">
+                <FontAwesomeIcon icon="arrow-left" />
+              </Link>
+              <Link to="/books" className="book__link">
+                <h2 className="book__selected--title--top">Books</h2>
+              </Link>
+            </div>
+            <div className="book__selected">
+              <figure className="book__selected--figure">
+                <img className="book__selected--img" src={book.url} alt="" />
+              </figure>
+              <div className="book__selected--description">
+                <h2 className="book__selected--title">{book.title}</h2>
+                <div className="book__selected--price">
+                </div>
+                <div className="book__summary">
+                  <h3 className="book__summary--title">Summary</h3>
+                  <p className="book__summary--para">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Veniam, repellendus modi odio porro, consequuntur,
+                    asperiores minima sint voluptatem at reiciendis ducimus
+                    neque provident alias iure nihil explicabo nobis id
+                    voluptas.
+                  </p>
+                  <p className="book__summary--para">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Veniam, repellendus modi odio porro, consequuntur,
+                    asperiores minima sint voluptatem at reiciendis ducimus
+                    neque provident alias iure nihil explicabo nobis id
+                    voluptas.
+                  </p>
+                </div>
+                <button className="btn" onClick={() => addItemToCart(book)}>
+                  Add to Cart
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="books__container">
+          <div className="row">
+            <div className="book__selected--top">
+              <h2 className="book__selected--title--top">Recommended Books</h2>
+            </div>
+          </div>
+        </div>
+      </main>
+    </div>
+  );
+};
+
+export default BookInfo;
